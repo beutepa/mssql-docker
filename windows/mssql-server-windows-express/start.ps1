769f2c4a0f3853e3af07e5f9003ce6f2d00497a8
beutepa <pascal.beutels@gmail.com>
2017-03-29 15:09:12 +0200

    -Untested- Support backup with multiple files
    Get as much information from the backup itself to restore.

--- windows/mssql-server-windows-express/start.ps1
+++ windows/mssql-server-windows-express/start.ps1
@@ -3,6 +3,7 @@
 # The format for attach_dbs
 # The format for restore_dbs
 
+
 param(
 [Parameter(Mandatory=$false)]
 [string]$sa_password,
@@ -35,22 +36,38 @@ if($sa_password -ne "_"){
     Invoke-Sqlcmd -Query $sqlcmd -ServerInstance ".\SQLEXPRESS" 
 }
 
+#--env restore_dbs="[{'dbName': 'ISHDB-12.0.0','dbBackupFile': 'C:\\sqlexpress\\backup\\ISH-12.0.0-sqlserver2012.MobilePhones.bak'}]"
 $restore_dbs_cleaned = $restore_dbs.TrimStart('\\').TrimEnd('\\')
 
 $dbs = $restore_dbs_cleaned | ConvertFrom-Json
 
 if ($null -ne $dbs -And $dbs.Length -gt 0){
-	Write-Verbose "Restoring $($dbs.Length) database(s)"
+    $svrConn = new-object Microsoft.SqlServer.Management.Common.ServerConnection
+    $svrConn.LoginSecure = $true
+    $svr = new-object Microsoft.SqlServer.Management.Smo.Server ($svrConn)
+    $res = new-object Microsoft.SqlServer.Management.Smo.Restore
+
+	Write-Verbose "Restoring $($dbs.Length) database(s) to $($svr.Name)"
 	Foreach($db in $dbs)
 	{	
-		$RelocateData = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("$($db.dbName)", "c:\sqlexpress\data\$($db.dbName).mdf")
-		$RelocateLog = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("$($db.dbName)_Log", "c:\sqlexpress\data\$($db.dbName)_Log.ldf")
+        $res.Devices.AddDevice($db.dbBackupFile, [Microsoft.SqlServer.Management.Smo.DeviceType]::File)
+        $dt = $res.ReadFileList($svr)
+        $RelocateFile = @()
+        foreach($r in $dt.Rows)
+        {
+            $logicalFileName = $r["LogicalName"]
+            $physicalFileName = Split-Path $r["PhysicalName"] -leaf
+            $RelocateFile += New-Object Microsoft.SqlServer.Management.Smo.RelocateFile($logicalFileName, "c:\sqlexpress\data\$($physicalFileName)")
+        }
+		#$RelocateData = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("InfoShare", "c:\sqlexpress\data\$($db.dbName).mdf")
+		#$RelocateLog = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("InfoShare_Log", "c:\sqlexpress\data\$($db.dbName)_Log.ldf")
 
-		Write-Verbose "Restore-SqlDatabase -ServerInstance '.\SQLEXPRESS' -Database $db.dbName -BackupFile $db.dbBackupFile -RelocateFile @($RelocateData,$RelocateLog)"
-		Restore-SqlDatabase -ServerInstance ".\SQLEXPRESS" -Database $db.dbName -BackupFile $db.dbBackupFile -RelocateFile @($RelocateData,$RelocateLog)
+		Write-Verbose "Restore-SqlDatabase -ServerInstance '.\SQLEXPRESS' -Database $db.dbName -BackupFile $db.dbBackupFile -RelocateFile $RelocateFile"
+		Restore-SqlDatabase -ServerInstance ".\SQLEXPRESS" -Database $db.dbName -BackupFile $db.dbBackupFile -RelocateFile $RelocateFile
 	}
 }
 
+#--env attach_dbs="[{'dbName': 'IShDita_130','dbFiles': ['C:\\sqlexpress\\data\\IShDita_130.mdf', 'C:\\sqlexpress\\data\\IShDita_130.ldf']}]"
 $attach_dbs_cleaned = $attach_dbs.TrimStart('\\').TrimEnd('\\')
 
 $dbs = $attach_dbs_cleaned | ConvertFrom-Json
@@ -76,9 +93,11 @@ if ($null -ne $dbs -And $dbs.Length -gt 0){
 Write-Verbose "Started SQL Server."
 
 $lastCheck = (Get-Date).AddSeconds(-2) 
-
+<#
 while ($true) { 
     Get-EventLog -LogName Application -Source "MSSQL*" -After $lastCheck | Select-Object TimeGenerated, EntryType, Message	 
     $lastCheck = Get-Date 
     Start-Sleep -Seconds 2 
 }
+#>
+powershell
