c873b92823a5cd5072292f3d93da7b5cf1f813db
beutepa <pascal.beutels@gmail.com>
2017-05-05 07:41:34 +0000

    Multiple databases support

--- windows/mssql-server-windows-express/readme.md
+++ windows/mssql-server-windows-express/readme.md
@@ -75,6 +75,7 @@ The image provides three environment variables to optionally set: </br>
 	The path refers to files **within the container**. So make sure to include them in the image or mount them via **-v**!
 	If the dbDataPath or dbLogPath are not provided, the default values as specified in the database settings will be used.
 	(TODO) There will also be an option to force to replace the database, for the moment the script fails if the files to restore (the .mdf, .ldf files) already exist in the restore location.
+	(TODO) Only backups containing type D (= Database) and/or L (= Log) are supported. Not yet supported I = Differential database, F = File or filegroup, G =Differential file, P = Partial, Q = Differential partial, NULL.
 
 
 - **attach_dbs**: The configuration for attaching custom DBs (.mdf, .ldf files).
--- windows/mssql-server-windows-express/start.ps1
+++ windows/mssql-server-windows-express/start.ps1
@@ -20,8 +20,8 @@ param(
 
 
 if($ACCEPT_EULA -ne "Y" -And $ACCEPT_EULA -ne "y"){
-	Write-Verbose "ERROR: You must accept the End User License Agreement before this container can start."
-	Write-Verbose "Set the environment variable ACCEPT_EULA to 'Y' if you accept the agreement."
+    Write-Verbose "ERROR: You must accept the End User License Agreement before this container can start."
+    Write-Verbose "Set the environment variable ACCEPT_EULA to 'Y' if you accept the agreement."
 
     exit 1 
 }
@@ -31,7 +31,7 @@ Write-Verbose "Starting SQL Server"
 start-service MSSQL`$SQLEXPRESS
 
 if($sa_password -ne "_"){
-	Write-Verbose "Changing SA login credentials"
+    Write-Verbose "Changing SA login credentials"
     $sqlcmd = "ALTER LOGIN sa with password=" +"'" + $sa_password + "'" + ";ALTER LOGIN sa ENABLE;"
     Invoke-Sqlcmd -Query $sqlcmd -ServerInstance ".\SQLEXPRESS" 
 }
@@ -47,13 +47,15 @@ if ($null -ne $dbs -And $dbs.Length -gt 0){
     $svrConn.LoginSecure = $true
     $svr = new-object Microsoft.SqlServer.Management.Smo.Server ($svrConn)
     $defaultDbDataPath = if ($svr.Settings.DefaultFile) { $svr.Settings.DefaultFile } else { $svr.MasterDBPath }
-    $defaultDbLogPath = if ($svr.Settings.DefaultLog) { $svr.Settings.DefaultLog } else { $svr.MasterDBLogPath }
-    $res = new-object Microsoft.SqlServer.Management.Smo.Restore
+    Write-Verbose "Default Data folder $($defaultDbDataPath)"
+    $defaultDbLogPath = if ($svr.Settings.DefaultLog) { $svr.Settings.DefaultLog } else { $svr.MasterDBLogPath }    
+    Write-Verbose "Default Log folder $($defaultDbLogPath)"
 
-	Write-Verbose "Restoring $($dbs.Length) database(s) to $($svr.Name)"
-	Foreach($db in $dbs)
-	{	
-		Write-Verbose "Restoring $($db)"
+    Write-Verbose "Restoring $($dbs.Length) database(s) to $($svr.Name)"
+    Foreach($db in $dbs)
+    {   
+        $res = new-object Microsoft.SqlServer.Management.Smo.Restore
+        Write-Verbose "Restoring $($db)"
         $res.Devices.AddDevice($db.dbBackupFile, [Microsoft.SqlServer.Management.Smo.DeviceType]::File)
         $dt = $res.ReadFileList($svr)
         $RelocateFile = @()
@@ -61,20 +63,20 @@ if ($null -ne $dbs -And $dbs.Length -gt 0){
         Write-Verbose "Using Data folder $($dbDataPath)"
         $dbLogPath = if ($db.dbLogPath) { $db.dbLogPath } else { $defaultDbLogPath }
         Write-Verbose "Using Log folder $($dbLogPath)"
-		
+
         foreach($r in $dt.Rows)
         {
             $logicalFileName = $r["LogicalName"]
             $type = $r["Type"]
             $physicalFilePath = if ($type -eq "D") { $dbDataPath.TrimEnd('\\') } else { $dbLogPath.TrimEnd('\\') }
             $physicalFileName = Split-Path $r["PhysicalName"] -leaf
-            Write-Verbose "RelocateFile with LogicalFileName:  $($logicalFileName), PhysicalName: $($physicalFileName)"
+            Write-Verbose "RelocateFile with LogicalFileName:  $($logicalFileName), PhysicalName: $($physicalFilePath)\$($physicalFileName)"
             $RelocateFile += New-Object Microsoft.SqlServer.Management.Smo.RelocateFile($logicalFileName, "$($physicalFilePath)\$($physicalFileName)")
         }
 
-		Write-Verbose "Restore-SqlDatabase -ServerInstance '.\SQLEXPRESS' -Database $db.dbName -BackupFile $db.dbBackupFile -RelocateFile $RelocateFile"
-		Restore-SqlDatabase -ServerInstance ".\SQLEXPRESS" -Database $db.dbName -BackupFile $db.dbBackupFile -RelocateFile $RelocateFile
-	}
+            Write-Verbose "Restore-SqlDatabase -ServerInstance '.\SQLEXPRESS' -Database $db.dbName -BackupFile $db.dbBackupFile -RelocateFile $RelocateFile"
+            Restore-SqlDatabase -ServerInstance ".\SQLEXPRESS" -Database $db.dbName -BackupFile $db.dbBackupFile -RelocateFile $RelocateFile
+    }
 }
 
 #--env attach_dbs="[{'dbName': 'IShDita_130','dbFiles': ['C:\\sqlexpress\\data\\IShDita_130.mdf', 'C:\\sqlexpress\\data\\IShDita_130.ldf']}]"
@@ -83,21 +85,21 @@ $attach_dbs_cleaned = $attach_dbs.TrimStart('\\').TrimEnd('\\')
 $dbs = $attach_dbs_cleaned | ConvertFrom-Json
 
 if ($null -ne $dbs -And $dbs.Length -gt 0){
-	Write-Verbose "Attaching $($dbs.Length) database(s)"
-	Foreach($db in $dbs)
-	{
-		$files = @();
-		Foreach($file in $db.dbFiles)
-		{
-			$files += "(FILENAME = N'$($file)')";
-		}
-		
-		$files = $files -join ","
-		$sqlcmd = "sp_detach_db ""$($db.dbName)"";CREATE DATABASE ""$($db.dbName)"" ON $($files) FOR ATTACH ;"
+    Write-Verbose "Attaching $($dbs.Length) database(s)"
+    Foreach($db in $dbs)
+    {
+        $files = @();
+        Foreach($file in $db.dbFiles)
+        {
+            $files += "(FILENAME = N'$($file)')";
+        }
+        
+        $files = $files -join ","
+        $sqlcmd = "sp_detach_db ""$($db.dbName)"";CREATE DATABASE ""$($db.dbName)"" ON $($files) FOR ATTACH ;"
 
-		Write-Verbose "Invoke-Sqlcmd -Query $($sqlcmd) -ServerInstance '.\SQLEXPRESS'"
-		Invoke-Sqlcmd -Query $sqlcmd -ServerInstance ".\SQLEXPRESS"
-	}
+        Write-Verbose "Invoke-Sqlcmd -Query $($sqlcmd) -ServerInstance '.\SQLEXPRESS'"
+        Invoke-Sqlcmd -Query $sqlcmd -ServerInstance ".\SQLEXPRESS"
+    }
 }
 
 Write-Verbose "Started SQL Server."
@@ -105,7 +107,7 @@ Write-Verbose "Started SQL Server."
 $lastCheck = (Get-Date).AddSeconds(-2) 
 <#
 while ($true) { 
-    Get-EventLog -LogName Application -Source "MSSQL*" -After $lastCheck | Select-Object TimeGenerated, EntryType, Message	 
+    Get-EventLog -LogName Application -Source "MSSQL*" -After $lastCheck | Select-Object TimeGenerated, EntryType, Message   
     $lastCheck = Get-Date 
     Start-Sleep -Seconds 2 
 }
